7b1960182d059ebc54bd4d15fdf854811edb6c1e
Robert McGuire <robert.m.mcguire@gmail.com>
2009-01-18 12:24:54 -0500

    Removing redundant param hashing (Sinatra does it now)

--- app.rb
+++ app.rb
@@ -24,23 +24,6 @@ helpers do
   include Sinatra::Authorization 
 end
 
-# Parse params Rails-style
-# http://sinatra.rubyforge.org/book.html#handling_of_rails_like_nested_params
-before do
-  new_params = {}
-  params.each_pair do |full_key, value|
-    this_param = new_params
-    split_keys = full_key.split(/\]\[|\]|\[/)
-    split_keys.each_index do |index|
-      break if split_keys.length == index + 1
-      this_param[split_keys[index]] ||= {}
-      this_param = this_param[split_keys[index]]
-   end
-   this_param[split_keys.last.to_sym] = value
-  end
-  request.params.replace new_params
-end
-
 # Feeds control panel thing
 get "/" do
   require_administrative_privileges
